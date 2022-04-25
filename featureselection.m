3d89a8636e7d347f0bec85948f0fde58bade55df
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-04-25 15:58:54 +0200

    svm and feature ranking

--- featureselection.m
+++ featureselection.m
@@ -1,11 +1,48 @@
 % Read data from file
-originaldata = readtable(['Peptidome2_240x60.xlsx']);
-data = originaldata(:, 2:end);
+originaldata = readtable('peptidome2_240.csv');
 
-! 
-cv = cvpartition(size(data,2),'HoldOut',0.3);
-idx = cv.test;
+%seprate names from numeric data
+features_names = originaldata(2:end, 1);
+labels = originaldata(1, 2:end);
+data = originaldata(2:end, 2:end);
+
+%convert to matrics
+numdata = table2array(data);
+labels = table2array(labels);
+
 % Separate to training and test data
-dataTrain = data(:, ~idx);
-dataTest  = data(2:end, idx);
+cv = cvpartition(size(numdata,2),'HoldOut',0.3);
+idx = cv.test;
+%transpose() because the SVM works with rows as different cases
+dataTrain = transpose(numdata(:, ~idx));
+classTrain = transpose(labels(:, ~idx));
+dataTest  = transpose(numdata(:, idx));
+classTest = transpose(labels(:, idx));
+
+%[Mdl, fitinfo] = fitclinear(dataTrain, classTrain);
+Mdl = fitcsvm(dataTrain, classTrain, 'KernelFunction','linear');
+test_labels = predict(Mdl, dataTest);
+
+importance = array2table(Mdl.Beta);
+peptide_imp = [features_names, importance];
+peptide_imp = sortrows(peptide_imp, "Var1", "descend");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
