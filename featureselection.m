d0f96efb137f7f52cca57213604ec412a5902490
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-05-01 11:00:15 +0200

    RFE added

--- featureselection.m
+++ featureselection.m
@@ -1,45 +1,56 @@
-% Read data from file
-originaldata = readtable('peptidome2_240.csv');
+clear
+clc
 
-%seprate names from numeric data
-features_names = originaldata(2:end, 1);
-labels = originaldata(1, 2:end);
-data = originaldata(2:end, 2:end);
+% Odczyt danych z pliku
+originaldata = readtable('peptidome2_240.csv','VariableNamingRule','preserve');
 
-%convert to matrics
-numdata = table2array(data);
-labels = table2array(labels);
+data_name = originaldata.Properties.VariableNames{1};
+originaldata.Properties.VariableNames{1} = 'Feature';
+labels = table2array(originaldata(1, 2:end));
 
-% Separate to training and test data
-cv = cvpartition(size(numdata,2),'HoldOut',0.3);
-idx = cv.test;
-%transpose() because the SVM works with rows as different cases
-dataTrain = transpose(numdata(:, ~idx));
-classTrain = transpose(labels(:, ~idx));
-dataTest  = transpose(numdata(:, idx));
-classTest = transpose(labels(:, idx));
+% Określenie liczby pętli oraz eliminowanych cech w każdej pętli
+loops = 5;
+k = 10;
 
-%[Mdl, fitinfo] = fitclinear(dataTrain, classTrain);
-Mdl = fitcsvm(dataTrain, classTrain, 'KernelFunction','linear');
-test_labels = predict(Mdl, dataTest);
+if loops*k > size(originaldata,1)-2
+    error('Za duża liczba eliminowanych cech. Zmień wartość loops lub k.')
+end
 
-importance = array2table(Mdl.Beta);
-peptide_imp = [features_names, importance];
-peptide_imp = sortrows(peptide_imp, "Var1", "descend");
+for j = 1:loops
+    
+    % Rozdzielenie zmiennych z tabeli danych
+    features_names = originaldata(2:end, 1);
+    data = table2array(originaldata(2:end, 2:end));
 
+    % Podział danych do trenowania i testowania (cv - cross-validation)
+    cv = cvpartition(size(data,2),'HoldOut',0.3);
+    idx = cv.test;
+    
+    % transpozycja macierzy - SVM przyjmuje wiersze jako próbki
+    dataTrain = transpose(data(:, ~idx));
+    classTrain = transpose(labels(:, ~idx));
+    dataTest  = transpose(data(:, idx));
+    classTest = transpose(labels(:, idx));
 
+    % [Mdl, fitinfo] = fitclinear(dataTrain, classTrain);
+    Mdl = fitcsvm(dataTrain, classTrain, 'KernelFunction', 'linear');
+    test_labels = predict(Mdl, dataTest);
+    
+    e1 = sum(logical(transpose(test_labels)-labels(idx)))/cv.TestSize;
+    %SVM_error = (e1 + e2 + e3)/3;
 
+    % Posortowanie cech ze względu na ich kryterium ważności
+    criterium = array2table(Mdl.Beta.^2);
+    peptide_imp = [features_names, criterium];
+    peptide_imp = sortrows(peptide_imp, "Var1", "descend");
 
-
-
-
-
-
-
-
-
-
-
+    % Usunięcie k najmniej znaczących cech
+    for i = 1:k
+        name = string(table2cell(peptide_imp(end,1)));
+        originaldata(originaldata.Feature == name,:) = [];
+        peptide_imp(end,:) = [];
+    end
+end
 
 
 
