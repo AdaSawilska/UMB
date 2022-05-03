2dde699f285cdbdd3dfd78de7e93ab0123e98d20
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-05-03 23:11:31 +0200

    version fix

--- featureselection.m
+++ featureselection.m
@@ -6,38 +6,67 @@ originaldata = readtable('peptidome2_240.csv','VariableNamingRule','preserve');
 
 data_name = originaldata.Properties.VariableNames{1};
 originaldata.Properties.VariableNames{1} = 'Feature';
-FISHERdata = originaldata;
 labels = table2array(originaldata(1, 2:end));
+P = size(originaldata,1)-1;
 
-% Określenie liczby pętli oraz eliminowanych cech w każdej pętli
-loops = 23;
-x = 10;
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Fisher score na originaldata (algorytm RFE usuwa cechy z originaldata)
 
-if loops*x > size(originaldata,1)-2
-    error('Za duża liczba eliminowanych cech. Zmień wartość loops lub x.')
-end
+features_names = originaldata(2:end, 1);
+data = table2array(originaldata(2:end, 2:end));
+cv = cvpartition(size(data,2),'HoldOut',0.3);
+   
+idx = cv.test;
+% transpozycja macierzy - SVM przyjmuje wiersze jako próbki
+dataTrain = transpose(data(:, ~idx));
+classTrain = transpose(labels(:, ~idx));
+dataTest  = transpose(data(:, idx));
+classTest = transpose(labels(:, idx));
 
-% SVM + RFE
-for i = 1:loops
+[index,scores] = fsrftest(dataTrain, classTrain);
+
+% Posortowanie cech ze względu na ich kryterium ważności
+fisher_peptide_imp = sortrows([index; scores].', 1, "ascend");
+fisher_peptide_imp = sortrows([features_names, array2table(fisher_peptide_imp)], 3, "descend");
+
+
+
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Liczba eliminowanych cech w każdej pętli algorytmu RFE
+x = 1;
+
+% Liczba usuniętych cech (do porównania błędów klasyfikacji)
+elim = [1 10 20 50 100];
+
+% Końcowa liczba najważniejszych cech
+z = 100;
+
+e = zeros(1,3);
+E = [];
+
+while height(originaldata)-1 > z
     
     % Rozdzielenie zmiennych z tabeli danych
     features_names = originaldata(2:end, 1);
     data = table2array(originaldata(2:end, 2:end));
 
     % Podział danych do trenowania i testowania (cv - cross-validation)
-    %cv = cvpartition(size(data,2),'HoldOut',0.3);
-    %idx = cv.test;
     cv = cvpartition(size(data,2),'KFold',3);
     
     for j = 1:3
+        
+        % Indeksy próbek do testowania (~idx - do trenowania)
         idx = test(cv,j);
 
-        % transpozycja macierzy - SVM przyjmuje wiersze jako próbki
+        % Transpozycja macierzy (SVM przyjmuje wiersze jako próbki)
         dataTrain = transpose(data(:, ~idx));
         classTrain = transpose(labels(:, ~idx));
         dataTest  = transpose(data(:, idx));
         classTest = transpose(labels(:, idx));
         
+        % Trening oraz test klasyfikatora
         if(j ==1)
             Mdl = fitcsvm(dataTrain, classTrain, 'KernelFunction', 'linear');
             IncrementalMdl = incrementalLearner(Mdl);
@@ -45,63 +74,29 @@ for i = 1:loops
             IncrementalMdl = fit(IncrementalMdl, dataTrain, classTrain);
         end
         test_labels = predict(IncrementalMdl, dataTest);
-        e(j) = sum(logical(transpose(test_labels)-labels(idx)))/cv.TestSize(j);        
+
+        % Błąd klasyfikatora
+        e(j) = sum(logical(transpose(test_labels)-labels(idx)))/cv.TestSize(j);
     end
     
-    %SVM_error = mean(e);
-
+    % Jeżeli liczba usuniętych cech zgadza się z wartością z wektora elim,
+    % zapisywany jest błąd klasyfikatora
+    if ismember(P-height(features_names),elim)
+        E(end+1) = mean(e);
+    end
+    
     % Posortowanie cech ze względu na ich kryterium ważności
-    criterium = array2table(Mdl.Beta.^2);
-    svm_peptide_imp = [features_names, criterium];
-    svm_peptide_imp = sortrows(svm_peptide_imp, "Var1", "descend");
+    criterium = array2table(IncrementalMdl.Beta.^2);
+    peptide_imp = [features_names, criterium];
+    peptide_imp = sortrows(peptide_imp, "Var1", "descend");
 
     % Usunięcie x najmniej znaczących cech
     for j = 1:x
-        name = string(table2cell(svm_peptide_imp(end,1)));
+        name = string(table2cell(peptide_imp(end,1)));
         originaldata(originaldata.Feature == name,:) = [];
-        svm_peptide_imp(end,:) = [];
+        peptide_imp(end,:) = [];
     end
 end
-
-
-% F-score
-for i = 1:loops
-
-    % Rozdzielenie zmiennych z tabeli danych
-    features_names = FISHERdata(2:end, 1);
-    data = table2array(FISHERdata(2:end, 2:end));
-
-    % Podział danych do trenowania i testowania (cv - cross-validation)
-    cv = cvpartition(size(data,2),'HoldOut',0.3);
-   
-    idx = cv.test;
-    % transpozycja macierzy - SVM przyjmuje wiersze jako próbki
-    dataTrain = transpose(data(:, ~idx));
-    classTrain = transpose(labels(:, ~idx));
-    dataTest  = transpose(data(:, idx));
-    classTest = transpose(labels(:, idx));
-
-    [index,scores] = fsrftest(dataTrain, classTrain);
-
-    % Posortowanie cech ze względu na ich kryterium ważności
-    fisher_peptide_imp = sortrows([index; scores].', 1, "ascend");
-    fisher_peptide_imp = sortrows([features_names, array2table(fisher_peptide_imp)], 3, "descend");
-
-    % Usunięcie x najmniej znaczących cech
-    for j = 1:x
-        name = string(table2cell(fisher_peptide_imp(end,1)));
-        FISHERdata(FISHERdata.Feature == name,:) = [];
-        fisher_peptide_imp(end,:) = [];
-    end
-end
-
-
-
-
-
-
-
-
 
 
 
