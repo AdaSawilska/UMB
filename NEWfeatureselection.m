605e82aa93f981d294866b4c1c050108f33b93e1
AdaSawilska <84930128+AdaSawilska@users.noreply.github.com>
2022-05-06 19:26:26 +0200

    expression matrix

--- NEWfeatureselection.m
+++ NEWfeatureselection.m
@@ -26,80 +26,11 @@ classes = transpose(labels);
 % Posortowanie cech ze względu na ich kryterium ważności
 fisher_peptide_imp = sortrows([index; scores].', 1, "ascend");
 fisher_peptide_imp = sortrows([features_names, array2table(fisher_peptide_imp)], 3, "descend");
-f_score = fisher_peptide_imp(1:important_features, :);
+fisher_peptide_index = fisher_peptide_imp(1:important_features, 2);
+expression_matrix = originaldata(2:end, : );
+f_score = expression_matrix(table2array(fisher_peptide_index),:);
 
 end
-
-
-
-
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-% Liczba eliminowanych cech w każdej pętli algorytmu RFE
-% x = 1;
-% 
-% % Liczba usuniętych cech (do porównania błędów klasyfikacji)
-% elim = [1 10 20 50 100];
-% 
-% % Końcowa liczba najważniejszych cech
-% z = 100;
-% 
-% e = zeros(1,3);
-% E = [];
-% 
-% while height(originaldata)-1 > z
-%     
-%     % Rozdzielenie zmiennych z tabeli danych
-%     features_names = originaldata(2:end, 1);
-%     data = table2array(originaldata(2:end, 2:end));
-% 
-%     % Podział danych do trenowania i testowania (cv - cross-validation)
-%     cv = cvpartition(size(data,2),'KFold',3);
-%     
-%     for j = 1:3
-%         
-%         % Indeksy próbek do testowania (~idx - do trenowania)
-%         idx = test(cv,j);
-% 
-%         % Transpozycja macierzy (SVM przyjmuje wiersze jako próbki)
-%         dataTrain = transpose(data(:, ~idx));
-%         classTrain = transpose(labels(:, ~idx));
-%         dataTest  = transpose(data(:, idx));
-%         classTest = transpose(labels(:, idx));
-%         
-%         % Trening oraz test klasyfikatora
-%         if(j ==1)
-%             Mdl = fitcsvm(dataTrain, classTrain, 'KernelFunction', 'linear');
-%             IncrementalMdl = incrementalLearner(Mdl);
-%         else
-%             IncrementalMdl = fit(IncrementalMdl, dataTrain, classTrain);
-%         end
-%         test_labels = predict(IncrementalMdl, dataTest);
-% 
-%         % Błąd klasyfikatora
-%         e(j) = sum(logical(transpose(test_labels)-labels(idx)))/cv.TestSize(j);
-%     end
-%     
-%     % Jeżeli liczba usuniętych cech zgadza się z wartością z wektora elim,
-%     % zapisywany jest błąd klasyfikatora
-%     if ismember(P-height(features_names),elim)
-%         E(end+1) = mean(e);
-%     end
-%     
-%     % Posortowanie cech ze względu na ich kryterium ważności
-%     criterium = array2table(IncrementalMdl.Beta.^2);
-%     peptide_imp = [features_names, criterium];
-%     peptide_imp = sortrows(peptide_imp, "Var1", "descend");
-% 
-%     % Usunięcie x najmniej znaczących cech
-%     for j = 1:x
-%         name = string(table2cell(peptide_imp(end,1)));
-%         originaldata(originaldata.Feature == name,:) = [];
-%         peptide_imp(end,:) = [];
-%     end
-% end
-% 
-
 
 
 
